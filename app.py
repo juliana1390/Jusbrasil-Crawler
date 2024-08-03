f71b34b731007365ce63cea1cc87e345f0e4bef9
juliana1390 <juliana13290@gmail.com>
2024-08-03 20:03:57 -0300

    crawler tjal - retornando outros dados do processo

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, Response
 from crawlers import tjal_fetch_data
+import json
 
 app = Flask(__name__)
 
+# rota /api/process definida, aceitando apenas requisições POST
 @app.route('/api/process', methods=['POST'])
+
+# extrai os dados da requisicao
 def get_data():
     data = request.json
     nro_processo = data.get("nro_processo")
     tribunal = data.get("tribunal")
-    grau = data.get("grau", 1)
+    grau = data.get("grau")
 
-    if tribunal == "TJAL":
-        result = tjal_fetch_data(nro_processo, grau)
-    else:
-        return jsonify({"erro": "Tribunal nao abrangido pela busca"}), 400
-    return jsonify(result)
+    if tribunal != 'TJAL':
+        return Response(
+            json.dumps({"erro": "Tribunal não suportado"}),
+            mimetype='application/json; charset=utf-8'
+        ), 400
+    
+    result = tjal_fetch_data(nro_processo, grau)
+    
+    # conversao
+    json_response = json.dumps(result, ensure_ascii=False)
+
+    return Response(
+        json_response,
+        mimetype='application/json; charset=utf-8'
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
--- crawlers.py
+++ crawlers.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException
-
+import json
 
 # log para verificar successo ou falha
 logging.basicConfig(level=logging.INFO)
@@ -34,28 +34,43 @@ def tjal_fetch_data(nro_processo, grau):
     driver = driver_setup()
     driver.get(url)
 
+    data = {} # inicializa um dicionário para armazenar os dados
+
     try:
         logger.info(f"Acessando URL: {url}")
 
-        # esperando que a caixa de busca seja encontrada
+        # espera a caixa de busca ser encontrada
         search_box = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "numeroDigitoAnoUnificado"))
         )
-        logger.info("Caixa de busca encontrada.")
+        logger.info("Página carregada.")
+
         search_box.send_keys(nro_processo) # envia nro do processo
         search_box.send_keys(Keys.RETURN) # simula ENTER
         logger.info(f"A busca pelo processo {nro_processo} foi iniciada.")
 
-        # esperando que o elemento "classeProcesso" esteja presente
-        data = {}
-        data["classe"] = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "classeProcesso"))
-        ).text
+        # coletar dados do processo
+        data["classe"] = driver.find_element(By.ID, "classeProcesso").text
         logger.info("Classe do processo encontrada.")
-    
+
+        data["area"] = driver.find_element(By.ID, "areaProcesso").text
+        logger.info("Área do processo encontrada.")
+
+        data["assunto"] = driver.find_element(By.ID, "assuntoProcesso").text
+        logger.info("Assunto encontrado.")
+
+        data["data_distribuicao"] = driver.find_element(By.ID, "dataHoraDistribuicaoProcesso").text
+        logger.info("Data/Hora de distribuição do processo encontrada.")
+
+        data["juiz"] = driver.find_element(By.ID, "juizProcesso").text
+        logger.info("Juíz encontrado.")
+
+        data["valor_acao"] = driver.find_element(By.ID, "valorAcaoProcesso").text
+        logger.info("Valor da ação encontrado.")
+
     except TimeoutException as e:
-        logger.error(f"TimeoutException ocorreu: {e}", exc_info=True) # capturar o rastreamento da pilha
-        logger.debug(driver.page_source)  # pagina para depuracao
+        logger.error(f"TimeoutException ocorreu: {e}", exc_info=True) # exc_info: capturar o rastreamento da pilha
+        logger.debug(driver.page_source)  # depuracao da pagina
         data = {"erro": "TimeoutException: " + str(e)}
 
     except NoSuchElementException as e:
